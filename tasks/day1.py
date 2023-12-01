fdd9bd8fc8b1cc0eae682ed63f62b28249f38763
Gorbachev <a.gorbachev@cryptology.com>
2023-12-01 23:02:15 +0200

    switched to github account. task1.2 not works

--- tasks/day1.py
+++ tasks/day1.py
@@ -2,10 +2,57 @@
 --- Day 1: Trebuchet?! ---
 https://adventofcode.com/2023/day/1
 """
+import re
+
+from utils.helpers import HUMAN_DIGITS
 from utils.test_and_run import run, test
 
+_pattern = "|".join(f"({word})" for word in HUMAN_DIGITS)
+_DIGITS_REXP = re.compile(
+    rf"({_pattern})"
+)
+WORD2DIGIT = {
+    HUMAN_DIGITS[n]: n + 1 for n in range(len(HUMAN_DIGITS))
+}
 
-def trebuchet(inp, test_part=1):
+
+def trebuchet2(inp, **_):
+    res = 0
+    for line in inp:
+        # digits = _DIGITS_REXP.match(line).groups()[1:]
+        start = None
+        end = None
+        last_word = ""
+        for s in line:
+            v = None
+            if s.isdigit():
+                v = int(s)
+                last_word = ""
+            else:
+                last_word += s
+                if last_word in WORD2DIGIT:
+                    v = WORD2DIGIT[last_word]
+                    last_word = ""
+                elif not any((word.startswith(last_word) for word in WORD2DIGIT)):
+                    if len(last_word) == 1:
+                        last_word = ""
+                    else:
+                        last_word = s
+            if v:
+                if not start:
+                    start = v
+                else:
+                    end = v
+
+        if end is None:
+            end = start
+        val = (start * 10) + end
+        print(val)
+        res += val
+    return res
+
+
+def trebuchet(inp):
     res = 0
     for line in inp:
         v = None
@@ -14,9 +61,6 @@ def trebuchet(inp, test_part=1):
             reversed(line),
         ):
             for s in it:
-                if test_part == 2:
-                    a = 0
-
                 if s.isdigit():
                     if v is None:
                         v = 10 * int(s)
@@ -33,5 +77,5 @@ if __name__ == "__main__":
     test(trebuchet, expected=142)
     run(trebuchet)
 
-    test(trebuchet, test_part=2, expected=281)
-    run(trebuchet)
+    # test(trebuchet2, test_part=2, expected=281)
+    assert 54712 < run(trebuchet2) < 59556
---
+++ utils/helpers.py
@@ -0,0 +1,3 @@
+HUMAN_DIGITS = [
+    # "zero",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
