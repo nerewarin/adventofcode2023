ce5729877221ba44f96c5b26d5872bd560fcb766
Gorbachev <a.gorbachev@cryptology.com>
2023-12-02 12:47:00 +0200

    task2.1 refactoring

--- tasks/day2.py
+++ tasks/day2.py
@@ -5,6 +5,7 @@ https://adventofcode.com/2023/day/1
 import re
 from utils.test_and_run import run, test
 
+_REXP = re.compile(r'(\d+)\s*(\w+)')
 
 def cube_conundrum(inp, limit, **_):
     limit = dict(zip(("red", "green", "blue"), limit))
@@ -12,7 +13,7 @@ def cube_conundrum(inp, limit, **_):
     valid = []
     for i, line in enumerate(inp):
         # Extract color and number pairs using regular expression
-        matches = re.findall(r'(\d+)\s*(\w+)', line)
+        matches = _REXP.findall(line)
 
         # Create a dictionary to store the maximum number for each color
         max_numbers = {}
@@ -23,7 +24,6 @@ def cube_conundrum(inp, limit, **_):
             if color not in max_numbers or number > max_numbers[color]:
                 max_numbers[color] = number
 
-        print(max_numbers)
         for color, max_v in limit.items():
             if max_numbers[color] > max_v:
                 break
