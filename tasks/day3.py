e69868ff99a63ca8a8ef644add351a709fc37e97
Gorbachev <a.gorbachev@cryptology.com>
2023-12-03 21:15:05 +0200

    day3.2 ok

--- tasks/day3.py
+++ tasks/day3.py
@@ -1,8 +1,8 @@
 """
 --- Day 3: Gear Ratios  ---
-https://adventofcode.com/2023/day/3
-"""
+  """
 import re
+from collections import defaultdict
 from functools import reduce
 from operator import mul
 
@@ -10,6 +10,7 @@ from utils.test_and_run import run, test
 
 _NUMBERS = re.compile(r'(\d+)')
 _SYMBOLS = re.compile(r'[^0-9.]')
+
 
 def is_part(start, end, i, inp):
     row = inp[i]
@@ -19,10 +20,14 @@ def is_part(start, end, i, inp):
         if start > 0:
             left = inp[i][start - 1]
             if _SYMBOLS.match(left):
+                if left == "*":
+                    return i, start - 1
                 return True
         if end < l - 1:
             right = inp[i][end]
             if _SYMBOLS.match(right):
+                if right == "*":
+                    return i, end
                 return True
 
     if i > 0:
@@ -33,6 +38,8 @@ def is_part(start, end, i, inp):
                 if 0 < top_i < l - 1:
                     top = prev[top_i]
                     if _SYMBOLS.match(top):
+                        if top == "*":
+                            return i - 1, top_i
                         return True
 
     if i < l - 1:
@@ -43,25 +50,55 @@ def is_part(start, end, i, inp):
                 if 0 < bot_i < l - 1:
                     bot_el = bot[bot_i]
                     if _SYMBOLS.match(bot_el):
+                        if bot_el == "*":
+                            return i + 1, bot_i
                         return True
     return False
+
 
 def gear_ratios(inp, **_):
     valid = []
     for i, line in enumerate(inp):
-        matches = [(match.start(), match.end()) for match in _NUMBERS.finditer(line)]
+        numbers = [(match.start(), match.end()) for match in _NUMBERS.finditer(line)]
 
-        for start, end in matches:
+        for start, end in numbers:
             value = int(line[start: end])
             if is_part(start, end, i, inp):
                 valid.append(value)
-                print(value)
-            else:
-                print(-value)
+                # print(value)
+            # else:
+                # print(-value)
 
     return sum(valid)
+
+
+def gear_ratios2(inp, **_):
+    valid = []
+    gear_to_numbers = defaultdict(list)
+    for i, line in enumerate(inp):
+        numbers = [(match.start(), match.end()) for match in _NUMBERS.finditer(line)]
+
+        for start, end in numbers:
+            value = int(line[start: end])
+            is_a_part = is_part(start, end, i, inp)
+            if isinstance(is_a_part, tuple):
+                gear_pos = is_a_part
+                gear_to_numbers[gear_pos].append(value)
+                valid.append(value)
+            elif is_a_part is True:
+                valid.append(value)
+
+    res = 0
+    for gear, numbers in gear_to_numbers.items():
+        if len(numbers) == 2:
+            res += numbers[0] * numbers[1]
+
+    return res
 
 
 if __name__ == "__main__":
     test(gear_ratios, expected=4361)
     run(gear_ratios)
+
+    test(gear_ratios2, expected=467835)
+    run(gear_ratios2)
