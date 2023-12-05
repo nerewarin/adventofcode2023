e1925e462e093c81ed802d7d3beb0136b9100bc7
Gorbachev <a.gorbachev@cryptology.com>
2023-12-05 14:46:16 +0200

    day 5.1 initial

---
+++ tasks/day5.py
@@ -0,0 +1,17 @@
+"""
+--- Day 5: If You Give A Seed A Fertilizer  ---
+https://adventofcode.com/2023/day/5
+"""
+from utils.test_and_run import run, test
+
+
+def fertilizer(inp):
+    res = 0
+
+    return res
+
+
+if __name__ == "__main__":
+    test(fertilizer, expected=35)
+    run(fertilizer)
+
